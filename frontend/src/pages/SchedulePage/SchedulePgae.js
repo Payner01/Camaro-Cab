859a84584aac6e683eb058d0a8eb9ce06c8a980a
Bryce Payne <brycepayne@macbook-pro.lan>
2022-04-24 19:49:04 -0500

    started integrating first map on schedule page, working on bugs

--- frontend/src/pages/SchedulePage/SchedulePgae.js
+++ frontend/src/pages/SchedulePage/SchedulePgae.js
@@ -1,14 +1,25 @@
 import React from 'react';
 import useAuth from "../../hooks/useAuth";
 import { useEffect, useState } from "react";
+import { GoogleMap, useLoadScript, Marker } from "google-maps-react";
 
 const SchedulePage = (props) => {
     
     const [user, token] = useAuth();
+    const { isLoaded } = useLoadScript({googleMapsApiKey: ""})
+
+    if (!isLoaded) return <div>Loading...</div>;
+    
 
     return ( 
         <div>
             <h1>Hello Schedule Page</h1>
+            <GoogleMap 
+            zoom={10}
+            center={{ lat: 44, lng: -80 }}
+            mapContainerClassName="map-container">
+
+            </GoogleMap>
         </div>
      );
 }
